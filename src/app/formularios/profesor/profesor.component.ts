801e825db42688b0b8096df92bd6b00c0d4ca907
Naira Coppari <nairacoppari@gmail.com>
2021-12-02 19:18:51 -0300

    se creo respuesta del form

--- src/app/formularios/profesor/profesor.component.ts
+++ src/app/formularios/profesor/profesor.component.ts
@@ -10,36 +10,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ProfesorComponent implements OnInit {
 
   profesorInfo: FormGroup;
-  caracterInvalid:boolean;
-  caracterInvalidBis:boolean;
+  caracterInvalid: boolean;
+  caracterInvalidBis: boolean;
+  enviado: boolean;
 
   constructor(private fb: FormBuilder) {
     this.caracterInvalid = true;
     this.caracterInvalidBis = true;
-
+    this.enviado = false;
+    
     this.profesorInfo = this.fb.group({
-      nombreCompleto: ['' , Validators.required],
-      aniosDocencia: ['' , Validators.required],
-      materias: ['matematica' , Validators.required],
-      horaSemanales: ['10' , Validators.required],
-      alumnosACargo: ['' , Validators.required],
-      institucion: ['' , Validators.required],
-      email: ['' , [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      nombreCompleto: ['', Validators.required],
+      aniosDocencia: ['', Validators.required],
+      materias: ['matematica', Validators.required],
+      horaSemanales: ['Seleccione cantidad de horas', Validators.required],
+      alumnosACargo: ['', Validators.required],
+      institucion: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
     })
   }
 
-
   ngOnInit(): void {
-    console.log(this.profesorInfo.valid)
-  
+
   }
 
   onSubmit() {
     console.log(this.profesorInfo.value);
+    this.enviado = true;
+    this.onResetForm();
   }
 
-  keyPressNumbers(event: any){
-    
+  keyPressNumbers(event: any) {
     var charCode = (event.which) ? event.which : event.keyCode;
     // Only Numbers 0-9
     if ((charCode < 48 || charCode > 57)) {
@@ -54,7 +55,7 @@ export class ProfesorComponent implements OnInit {
     }
   }
 
-  onResetForm(){
+  onResetForm() {
     this.profesorInfo.reset();
   }
 
