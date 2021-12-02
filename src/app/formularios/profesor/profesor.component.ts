4fc698bde0ca035b8369df0bc2f8042abdcf3180
Naira Coppari <nairacoppari@gmail.com>
2021-12-02 17:39:37 -0300

    validaciones terminadas

--- src/app/formularios/profesor/profesor.component.ts
+++ src/app/formularios/profesor/profesor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,37 +9,53 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class ProfesorComponent implements OnInit {
 
-  //profesorInfo: FormGroup;
-
-  profesorInfo = this.fb.group({
-    nombreCompleto: ['' , Validators.required],
-    aniosDocencia: ['' , Validators.required],
-    materias: ['' , Validators.required],
-    horaSemanales: ['' , Validators.required],
-    alumnosACargo: ['' , Validators.required],
-    institucion: ['' , Validators.required],
-    email: ['' , Validators.required],
-  })
+  profesorInfo: FormGroup;
+  caracterInvalid:boolean;
+  caracterInvalidBis:boolean;
 
   constructor(private fb: FormBuilder) {
+    this.caracterInvalid = true;
+    this.caracterInvalidBis = true;
 
+    this.profesorInfo = this.fb.group({
+      nombreCompleto: ['' , Validators.required],
+      aniosDocencia: ['' , Validators.required],
+      materias: ['matematica' , Validators.required],
+      horaSemanales: ['10' , Validators.required],
+      alumnosACargo: ['' , Validators.required],
+      institucion: ['' , Validators.required],
+      email: ['' , [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+    })
   }
 
 
   ngOnInit(): void {
-    this.profesorInfo.valueChanges.subscribe({
-      next: (change) => {
-        console.log('es invalido el wacho', this.profesorInfo.invalid)
-        console.log('es valido el wacho', this.profesorInfo.valid)
-        console.log(change)
-
-      }
-    })
-
+    console.log(this.profesorInfo.valid)
+  
   }
 
   onSubmit() {
     console.log(this.profesorInfo.value);
+  }
+
+  keyPressNumbers(event: any){
+    
+    var charCode = (event.which) ? event.which : event.keyCode;
+    // Only Numbers 0-9
+    if ((charCode < 48 || charCode > 57)) {
+      event.preventDefault();
+      this.caracterInvalid = false;
+      this.caracterInvalidBis = false
+      return false;
+    } else {
+      this.caracterInvalid = true;
+      this.caracterInvalidBis = true;
+      return true;
+    }
+  }
+
+  onResetForm(){
+    this.profesorInfo.reset();
   }
 
 
